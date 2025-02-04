01c0ef5d928d59bcf1cf84a230a8ec8005b877e9
Garimas31 <garimassharma2003@gmail.com>
2025-02-04 12:32:08 +0530

    first commit

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7ork$ymtdx2@@$ia*n4*bn$hvij%!yh7^a**d8_yre7dofe%)8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
