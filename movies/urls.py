00ad1657f27a9ec583718bff87a7598af14b5bb4
Garimas31 <garimassharma2003@gmail.com>
2025-02-07 11:18:27 +0530

    first commit

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,5 @@ urlpatterns=[
     path('',views.movie_list,name='movie_list'),
     path('<int:movie_id>/theaters',views.theater_list,name='theater_list'),
     path('theater/<int:theater_id>/seats/book/',views.book_seats,name='book_seats'),
+    path('shows/', views.show_list, name='show_list'),
 ]
